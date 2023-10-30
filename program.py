cbaf2ac92f1a3b7ef35fb3b5d7645a1047f1c53c
Darwin Mazano <manza_92@yahoo.com>
2023-10-29 20:18:30 -0600

    agregando program.py

---
+++ program.py
@@ -0,0 +1,33 @@
+'''a = 93
+b = 27
+if a >= b:
+    print(a)
+
+    a = 24
+b = 44
+if a <= 0:
+    print(a)
+print(b)
+
+a = 27
+b = 93
+if a >= b:
+    print(a)
+else:
+    print(b)
+
+a = 27
+b = 93
+if a <= b:
+    print("a is less than or equal to b")
+elif a == b:
+    print("a is equal to b")'''
+
+a = 27
+b = 93
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else: 
+    print ("a is equal to b")
