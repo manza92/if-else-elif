212c1dca555c75897642721b477a771bfc7792a5
Darwin Mazano <manza_92@yahoo.com>
2023-10-29 20:32:43 -0600

    agregando comandos

--- program.py
+++ program.py
@@ -21,7 +21,7 @@ b = 93
 if a <= b:
     print("a is less than or equal to b")
 elif a == b:
-    print("a is equal to b")'''
+    print("a is equal to b")
 
 a = 27
 b = 93
@@ -31,3 +31,40 @@ elif a > b:
     print("a is greater than b")
 else: 
     print ("a is equal to b")
+
+a = 16
+b = 25
+c = 27
+if a > b:
+    if b > c:
+        print ("a is greater than b and b is greater than c")
+    else: 
+        print ("a is greater than b and less than c")
+elif a == b:
+    print ("a is equal to b")
+else:
+    print ("a is less than b")
+
+object_size = 10
+if object_size > 5:
+    print('We need to keep an eye on this object')
+else:
+    print('Object poses no threat.')
+
+a = 23
+b = 34
+if a == 34 or b == 34:
+    print(a + b)
+
+a = 23
+b = 34
+if a == 34 and b == 34:
+    print (a + b)'''
+
+object_size = 10
+proximity = 9000
+
+if object_size > 5 and proximity < 1000:
+    print('Evasive maneuvers required')
+else:
+    print('Object poses no threat')
