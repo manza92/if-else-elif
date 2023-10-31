b5241925056d4c254360364d39626c10fd6b32f1
Darwin Mazano <manza_92@yahoo.com>
2023-10-30 20:11:34 -0600

    agregando comandos

--- program.py
+++ program.py
@@ -59,7 +59,7 @@ if a == 34 or b == 34:
 a = 23
 b = 34
 if a == 34 and b == 34:
-    print (a + b)'''
+    print (a + b)
 
 object_size = 10
 proximity = 9000
@@ -68,3 +68,63 @@ if object_size > 5 and proximity < 1000:
     print('Evasive maneuvers required')
 else:
     print('Object poses no threat')
+
+fact = "The Moon has no atmosphere."
+two_facts = fact + "No sound can be heard on the Moon."
+print(two_facts)
+
+multiline = "Facts about the Moon:\nThere is no atmosphere.\nThere is no sound."
+print(multiline)
+
+multiline = """Facts about the Moon:
+ There is no atmosphere. 
+ There is no sound."""
+print(multiline)
+
+print("temperatures and facts about the moon".title()) 
+
+heading = "temperatures and facts about the moon"
+heading_upper = heading.title()
+print(heading_upper)  
+
+temperatures = "Daylight: 260 F Nighttime: -280 F"
+temperatures_list = temperatures.split()
+print(temperatures_list) 
+
+temperatures = "Daylight: 260 F\n Nighttime: -280 F"
+temperatures_list = temperatures.split('\n')
+print(temperatures_list) 
+
+print("Moon" in "This text will describe facts and challenges with space travel")
+
+print("Moon" in "This text will describe facts about the Moon") 
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Moon"))
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Mars"))
+
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.count("Mars"))
+print(temperatures.count("Moon"))
+
+print("The Moon And The Earth".lower())
+print("The Moon And The Earth".upper())
+
+print("The Moon And The Earth".lower().count("the")) 
+ 
+temperatures = "Mars Average Temperature: -60 C"
+parts = temperatures.split(':')
+print(parts)
+print(parts[-1]) 
+
+mars_temperature = "The highest temperature on Mars is about 30 C"
+for item in mars_temperature.split():
+    if item.isnumeric():
+        print(item)
+
+        print("-60".startswith('-')) '''
+
+if "30 C".endswith("C"):
+        print("This temperature is in Celsius")
